1b2257f794d6c6b9dd7fe83cd656b3d10ff2328d
Jonas395 <jonasgunnarsson99@gmail.com>
2025-10-06 14:09:33 +0200

    Fixed input directory

--- convert.py
+++ convert.py
@@ -5,8 +5,8 @@ from extensions import extensions
 from file_utility import get_folders
 input_directory, output_directory = get_folders(__file__)
 
-for file in input_directory:
-    if file.endswith(extensions.WEBP) or file.endswith(extensions.PNG):
+for file in os.listdir(input_directory):
+    if file.endswith(extensions.WEBP):
         image_path = os.path.join(input_directory, file)
         image = Image.open(image_path)
 
